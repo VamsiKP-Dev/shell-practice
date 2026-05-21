4bf6e5565de61be01fa87d8937de1c40de2a5af1
Vamsi Krishna P <vamsikrishnap@hotmail.com>
2026-05-21 12:59:03 +0530

    hello world

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump: Hey Iran, we are going blast you"
+echo "Iran: Yes, Please. You are welcome"
+echo "Trump: Hey Iran I am serious"
+echo "Iran: Hey Trump, we are also seious"
