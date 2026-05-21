165f9ee73f3f7e35d6df59d6ac22f16cd0ed161f
Vamsi Krishna P <vamsikrishnap@hotmail.com>
2026-05-21 13:23:45 +0530

    hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -4,6 +4,6 @@ PERSON1=Trump
 PERSON2=Iran
 
 echo "$PERSON1: Hey Ukraine, we are going blast you"
-echo "Iran: Yes, Please. You are welcome"
-echo "Trump: Hey Iran I am serious"
-echo "Iran: Hey Trump, we are also serious"
+echo "$PERSON2: Yes, Please. You are welcome"
+echo "$PERSON1: Hey $PERSON2 I am serious"
+echo "$PERSON2: Hey $PERSON1, we are also serious"
