8bf8da3002faf2a25ea5bf5c6511312b8018018d
Vamsi Krishna P <vamsikrishnap@hotmail.com>
2026-05-24 13:20:58 +0530

    hello world

---
+++ 08-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# gt -> Greater than
+# lt -> less than
+# ge -> Greater than or equal
+# eq -> equal
+# ne -> not equal
+# le -> Less than or equal
+if [ $NUMBER -ge 20 ]; then
+    echo "Given number $NUMBER is greater than or equal to 20"
+fi
