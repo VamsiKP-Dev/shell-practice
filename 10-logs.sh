73ac67b19597f3cc9dc7adbe1fc2ea55eddac22a
Vamsi Krishna P <vamsikrishnap@hotmail.com>
2026-05-25 10:41:46 +0530

    hello world

--- 10-logs.sh
+++ 10-logs.sh
@@ -14,7 +14,7 @@ fi
 # second arg -> exit code
 VALIDATE(){
     if [ $2 -ne 0 ]; then
-        echo "Installing $1 is ... FAILED"
+        echo "Installing $1 is ... FAILED" | tee -a $LOGS_FILE
         exit 1
     else
         echo "Installing $1 is SUCCESS"
